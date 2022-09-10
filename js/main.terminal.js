2265bb5fc9654adce8b7dee7f03381fe1d265e1e
Blank <69070442+blankminecraft@users.noreply.github.com>
2022-09-10 18:21:07 -0500

    added way to get to alpha page

--- js/main.terminal.js
+++ js/main.terminal.js
@@ -3,6 +3,10 @@ jQuery(function($) {
         start: function() {
             this.clear();
             redirect('game/');
+        },
+        alpha: function() {
+            this.clear();
+            redirect('alpha/');
         }
     }, {
         greetings: tutorial,

1b1f16dc30ec80c03c88ffd8cc0aec0e1164c66d
Blank <69070442+blankminecraft@users.noreply.github.com>
2022-09-23 18:17:27 -0500

    v1.0.1a: Added more to tutorial Made color white on tutorial

--- js/main.js
+++ js/main.js
@@ -12,44 +12,67 @@ const tutorial = `${white}Tutorial:
     ${darkGreen}<--Stats-->]
     This is how your stats should look
 
-    ${white}Stats: ]
-        ${white}Coins: ] ${yellow}{ 0 }]
-        ${white}Level: ]${green}{ 0 }]
-        ${white}XP: ]${darkGreen}{ 0/50 }]
-        ${white}Health: ]${red}{ 10/10 } ( +0 )]
-        ${white}Damage: ]${blue}{ 1 } ( +0 )]
-        ${white}Weapon: ]${purple}{ Fist }]
+    ${white}Stats:
+        ${white}Coins: ${yellow}{ 0.00 }]
+        ${white}Level: ${green}{ 0 }]
+        ${white}XP: ${darkGreen}{ 0.00/50.00 }]
+        ${white}HP: ${red}{ 10.00/10.00 } ( +0 )]
+        ${white}Regen: ${lightRedPink}{ 0.05/s } ( +0 ) ]
+        ${white}Damage: ${blue}{ 1.00 } ( +0 )]
+
+        ${white}Helmet: ${redPink}{ None }]
+        ${white}Chest: ${redPink}{ None }]
+        ${white}Legs: ${redPink}{ None }]
+        ${white}Boots: ${redPink}{ None }]
+
+        ${white}Weapon: ${purple}{ Fist }]
+        ${white}Offhand: ${purple}{ None }]
         
     These show you everything about your character.
     (Stats can be added as the game updates)
 
-    ${darkGreen}<--Weapons-->]
-    Weapons are displayed in your inventory
+    ${darkGreen}<--Weapons/Items-->]
+    Items are displayed in your inventory
 
     Open your inventory with ${blue}inv] command
 
     ${white}Inventory:
         {Fist}]
     
-    Your equiped weapon is shown in your stats in purple
+    Your equiped items is shown in your stats in ${purple}purple]/${redPink}redish pink]
 
-    ${white}Weapon: ]${purple}{ Fist }]
+    ${white}Helmet: ${redPink}{ None }]
+    ${white}Chest: ${redPink}{ None }]
+    ${white}Legs: ${redPink}{ None }]
+    ${white}Boots: ${redPink}{ None }]
 
-    You can equip other weapons with the ${blue}equip] command followed by the weapon name 
-    If its in your inventory itll be equiped
+    ${white}Weapon: ${purple}{ Fist }]
+    ${white}Offhand: ${purple}{ None }]
 
-    Weapons will be added as you find/buy them
-    Weapons can be dropped from enemys
-    Weapons can add stat boosts such as ${blue}+5 damage], or ${red}+5 health]
+    You can equip other items with the ${blue}equip] command followed by the item name 
+    If it's in your inventory it'll be equiped
 
-    You can inspect a certain weapon with the ${blue}inspect] command followed by the weapon name
-    You can show all weapons in the game with the ${blue}listItems] command
+    Items will be added as you find/buy them
+    Items can be dropped from enemys
+    Items can add stat boosts such as ${blue}+5 damage], or ${red}+5 health]
+
+    You can inspect a certain item with the ${blue}inspect] command followed by the item name
+    You can show all items in the game with the ${blue}listItems] command
+
+    ${darkGreen}<--Enemys-->]
+    You can start a fight with the ${blue}fight] command
+    Attack with the ${blue}attack] command
+    And try run away with the ${blue}run] command
+
+    If you die in battle then you'll lose half your coins, so try not to die!
+
+    If you win the battle, then you will gain some xp, coins, and maybe even a weapon/item
 
     ${darkGreen}<--Levels/XP-->]
     Your level and XP are shown in your stats in green
 
     ${white}Level: ]${green}{ 0 }]
-    ${white}XP: ]${darkGreen}{ 0/50 }]
+    ${white}XP: ]${darkGreen}{ 0.00/50.00 }]
 
     You earn XP as you win battles
     When your XP amount reaches its max, your level goes up by 1
